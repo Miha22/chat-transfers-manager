ef912d909e5b2ff162f42915d114dea1c7147eb3
Miha22 <deniel.alekseev0006@stud.hkr.se>
2022-08-17 01:36:23 +0300

    Still have a bug of language combination

--- tests/regex.cjs
+++ tests/regex.cjs
@@ -5,26 +5,29 @@ const strRus = 'х22 Apple iPhone 8 / SE 2020 šleife ar sistēmas konektoru mel
 const strEng = 'x1 Apple iPhone 8 / SE 2020 šleife ar sistēmas konektoru melns';
 const strReverseRus = '12х Apple iPhone 8 / SE 2020 LCD displejs ar skārienjūtīgo paneli un ramiti melns (Tianma AAA)';
 const strReverseEng = '2x Apple iPhone 8 / SE 2020 LCD displejs ar skārienjūtīgo paneli un ramiti melns (Tianma AAA)';
-const strCombined = `Nado: x2 USB flash disks GoodRam UMM3 32GBх3 USB flash disks HOCO UD9 Mini Car Music USB 2.0 64GB`;
-const strCombinedRev = `Nado: 2х USB flash disks GoodRam UMM3 32GB3X USB flash disks HOCO UD9 Mini Car Music USB 2.0 64GB`;
-const strCombined2 = `x1 USB flash disks GoodRam UMM3 32GB
+const strCombined = `Nado: 2x USB flash disks GoodRam UMM3 32GBх3 USB flash disks HOCO UD9 Mini Car Music USB 2.0 64GB`;
+const strCombinedRev = `Nado: х2 USB flash disks GoodRam UMM3 32GB3x USB flash disks HOCO UD9 Mini Car Music USB 2.0 64GB`;
+const strCombined2 = `x1 USB flash disks GoodRam UMM3 32GB или может еще
 x2 USB flash disks HOCO UD9 Mini Car Music USB 2.0 64GB
 x3 Bazookas for Aleksej`;
 
-console.log('X'.charCodeAt());// eng 'x' is 120 (hex 78), eng 'X' is 58 (hex 58)
-console.log('Х'.charCodeAt());// rus 'х' is 1093 (hex 445), rus 'Х' is 1061 (hex 425)
+//console.log('X'.charCodeAt());// eng 'x' is 120 (hex 78), eng 'X' is 58 (hex 58)
+//console.log('Х'.charCodeAt());// rus 'х' is 1093 (hex 445), rus 'Х' is 1061 (hex 425)
 //const pattern1 = /((\u0445|\u0425)[1-9][0-9]?.+)|((\u0078|\u0058)[1-9][0-9]?.+)/;
 const patternEng = /.*((\u0078|\u0058)[1-9][0-9]?.+(?=((\u0078|\u0058)[1-9][0-9]?)))/;//english xX, Xx, xx and XX
 const patternEngRev = /.*([1-9][0-9]?(\u0078|\u0058).+(?=([1-9][0-9]?(\u0078|\u0058))))/;//english reversed xX, Xx, xx and XX
-const patternRus = /.*((\u0445|\u0425)[1-9][0-9]?.+(?=((\u0445|\u0425)[1-9][0-9]?)))/;//russian xX, Xx, xx and XX
-const patternRusRev = /.*([1-9][0-9]?(\u0445|\u0425).+(?=([1-9][0-9]?(\u0445|\u0425))))/;//russian reversed xX, Xx, xx and XX
-const patternUnited = /.*((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?.+(?=((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?)))/;
-const patternUnitedRev = /.*([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425).+(?=([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425))))/;
-const pattern = /.*((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?.+(?=((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?)))|.*([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425).+(?=([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425))))/;
+//const patternRus = /.*((\u0445|\u0425)[1-9][0-9]?.+(?=((\u0445|\u0425)[1-9][0-9]?)))/;//russian xX, Xx, xx and XX
+//const patternRusRev = /.*([1-9][0-9]?(\u0445|\u0425).+(?=([1-9][0-9]?(\u0445|\u0425))))/;//russian reversed xX, Xx, xx and XX
+//const patternUnited = /.*((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?.+(?=((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?)))/;
+//const patternUnitedRev = /.*([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425).+(?=([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425))))/;
+//const pattern = /.*((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?.+(?=((\u0078|\u0058|\u0445|\u0425)[1-9][0-9]?)))|.*([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425).+(?=([1-9][0-9]?(\u0078|\u0058|\u0445|\u0425))))/;
 
-const matches = strCombinedRev.trim().split(pattern);
-const items = matches.filter((item) => (typeof item !== 'undefined' && item.length > 3));
-console.log(items);
+const str = strCombinedRev.replace(/х/g, "x").replace(/Х/g, "X");
+console.log(str.match(patternEngRev));
+
+// const matches = str.trim().split(patternEng);
+// const items = matches.filter((item) => (typeof item !== 'undefined' && item.length > 3));
+// console.log(items);
 
 class Item {
     constructor(name, quantity) {
